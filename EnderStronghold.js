4318b814fe2f056cc57e4e3e09362973bf6a92a8
Creepy0964 <59210160+Creepy0964@users.noreply.github.com>
2023-10-29 15:09:53 +0300

    Initial commit

---
+++ EnderStronghold.js
@@ -0,0 +1,45 @@
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+const rl = readline.createInterface({ input, output });
+
+let angle1, angle2, direction1, direction2;
+
+console.log('Welcome! This script will help you to calculate the distance to the Ender Stronghold.');
+console.log('NOTE: it is HIGHLY RECOMMENDED to read README.md before entering any data.');
+
+rl.question('Enter first angle: ', (answer) => {
+    if(isNaN(parseFloat(answer))) {
+        console.log(`Uh oh! Data type of answer isn't float. Exiting...`);
+        process.exit();
+    }
+    angle1 = answer;
+    rl.question('Enter second angle: ', (answer) => {
+        if(isNaN(parseFloat(answer))) {
+            console.log(`Uh oh! Data type of answer isn't float. Exiting...`);
+            process.exit();
+        }
+        angle2 = answer;
+        rl.question('Enter direction [-X/+X/-Z/+Z]: ', (answer) => {
+            direction1 = answer;
+            switch(direction1) {
+                case "-X":
+                    direction2 = '+Z';
+                    break;
+                case "+X":
+                    direction2 = '-Z';
+                    break;
+                case "-Z":
+                    direction2 = '+X';
+                    break;
+                case "+Z":
+                    direction2 = '-X';
+                    break;
+            }
+            rl.close();
+            let angle3 = Math.round((Math.abs(angle1 - angle2)) * 10) / 10;
+            let distance = 40 / (Math.sin(angle3 * (Math.PI / 180)));
+            console.log(`Done! Straight distance: ${Math.round(distance)}. Distance in blocks: ${Math.abs(Math.round(distance * Math.sin(angle2 * (Math.PI / 180))))}. Move more ${Math.abs(Math.round((distance * Math.sin(angle2 * (Math.PI / 180))) * Math.cos(angle2 * (Math.PI / 180))))} blocks in ${direction1}, then move ${Math.abs(Math.round((distance * Math.sin(angle2 * (Math.PI / 180))) * Math.sin(angle2 * (Math.PI / 180))))} blocks in ${direction2}.`);
+        });
+    });
+});
